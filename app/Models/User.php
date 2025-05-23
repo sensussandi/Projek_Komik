5250ab2c0c972f0a33e7311fce9758727ec502d4
SamuelJHS <Samuel.jhs8@gmail.com>
2025-05-23 11:08:05 +0700

    commit kedua

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that are masss assignable.
      *
      * @var list<string>
      */
