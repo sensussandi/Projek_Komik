41a17c1e6738a62d8bea940d6095db9ae0c78d00
nyukkiw <valentinobanyu0@gmail.com>
2025-06-16 20:30:08 +0700

    update temp

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -45,8 +45,8 @@
             </li>
           </ul>
 
-          <form form class="d-flex" method="GET" action="{{route('komik.search')}}">
-            <input class="form-control me-2" type="search" placeholder="Search" name="searching" aria-label="Search">
+          <form class="d-flex" method="GET" action="{{route('komik.search')}}">
+            <input class="form-control me-2" type="search" name="searching" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-light" type="submit">Search</button>
           </form>
         </div>
